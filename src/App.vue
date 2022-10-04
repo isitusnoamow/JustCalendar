8e9bcf7b2a69c8d8762034756a245df9e92d2a2a
isitusnoamow <240135@hkis.edu.hk>
2022-10-04 23:22:44 +0800

    using vue-cal

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Greet from "./components/Greet.vue";
+import VueCal from 'vue-cal'
+import '../node_modules/vue-cal/dist/vuecal.css'
+</script>
+
+<template>
+  <div class="container">
+    <vue-cal></vue-cal>
+  </div>
+</template>
+
+<style scoped>
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #249b73);
+}
+</style>
