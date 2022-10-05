8d43d75ce7ac918c1e46d404f3fa70cdd0f3265c
isitusnoamow <240135@hkis.edu.hk>
2022-10-05 23:51:06 +0800

    switched to fullcal

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,17 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Greet from "./components/Greet.vue";
-import VueCal from 'vue-cal'
-import '../node_modules/vue-cal/dist/vuecal.css'
+import FullCalendar from "@fullcalendar/vue3";
+import '@fullcalendar/core/vdom';
 </script>
 
 <template>
-  <div class="container">
-    <vue-cal></vue-cal>
-  </div>
+  <FullCalendar></FullCalendar>
 </template>
 
 <style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
+  FullCalendar{
+    height: 500px;
+    width: 100%;
+  }
 </style>
